611830535b0ad423ba4e60b7e4e5044dcd731b2d
dev1-pcp <mahmudamen@gmail.com>
2021-12-19 09:08:12 +0200

    remove wizerd

--- static/src/js/branch_reports.js
+++ static/src/js/branch_reports.js
@@ -1,4 +1,4 @@
-odoo.define('pcp_acc_nassag.account_report', function (require) {
+odoo.define('pcp_acc_nassag.branch_report', function (require) {
 'use strict';
 
 var core = require('web.core');
@@ -15,7 +15,6 @@ var Widget = require('web.Widget');
 
 var QWeb = core.qweb;
 var _t = core._t;
-
 var M2MFilters = Widget.extend(StandaloneFieldManagerMixin, {
     /**
      * @constructor
@@ -113,7 +112,7 @@ var M2MFilters = Widget.extend(StandaloneFieldManagerMixin, {
     },
 });
 
-var accountReportsWidget = AbstractAction.extend({
+var accountReportsWidget = AbstractAction.include({
     hasControlPanel: true,
 
     events: {
@@ -148,7 +147,7 @@ var accountReportsWidget = AbstractAction.extend({
         this.actionManager = parent;
         this.report_model = action.context.model;
         if (this.report_model === undefined) {
-            this.report_model = 'branch.report';
+            this.report_model = 'account.report';
         }
         this.financial_id = false;
         if (action.context.id) {
@@ -185,7 +184,7 @@ var accountReportsWidget = AbstractAction.extend({
         this.render();
 
         // A default value has been set for the filter accounts.
-        // Apply the filter to take this value into branch.
+        // Apply the filter to take this value into account.
         if("default_filter_accounts" in (this.odoo_context || {}))
             this.$('.o_account_reports_filter_input').val(this.odoo_context.default_filter_accounts).trigger("input");
     },
@@ -259,7 +258,7 @@ var accountReportsWidget = AbstractAction.extend({
             - the code.
             - the formula with values.
             - the domain.
-            - A button to show the branch.move.lines.
+            - A button to show the account.move.lines.
         */
 
         var self = this;
@@ -433,172 +432,6 @@ var accountReportsWidget = AbstractAction.extend({
         }).open();
     },
     render_searchview_buttons: function() {
-        var self = this;
-        // bind searchview buttons/filter to the correct actions
-        var $datetimepickers = this.$searchview_buttons.find('.js_account_reports_datetimepicker');
-        var options = { // Set the options for the datetimepickers
-            locale : moment.locale(),
-            format : 'L',
-            icons: {
-                date: "fa fa-calendar",
-            },
-        };
-        // attach datepicker
-        $datetimepickers.each(function () {
-            var name = $(this).find('input').attr('name');
-            var defaultValue = $(this).data('default-value');
-            $(this).datetimepicker(options);
-            var dt = new datepicker.DateWidget(options);
-            dt.replace($(this)).then(function () {
-                dt.$el.find('input').attr('name', name);
-                if (defaultValue) { // Set its default value if there is one
-                    dt.setValue(moment(defaultValue));
-                }
-            });
-        });
-        // format date that needs to be show in user lang
-        _.each(this.$searchview_buttons.find('.js_format_date'), function(dt) {
-            var date_value = $(dt).html();
-            $(dt).html((new moment(date_value)).format('ll'));
-        });
-        // fold all menu
-        this.$searchview_buttons.find('.js_foldable_trigger').click(function (event) {
-            $(this).toggleClass('o_closed_menu o_open_menu');
-            self.$searchview_buttons.find('.o_foldable_menu[data-filter="'+$(this).data('filter')+'"]').toggleClass('o_closed_menu');
-        });
-        // render filter (add selected class to the options that are selected)
-        _.each(self.report_options, function(k) {
-            if (k!== null && k.filter !== undefined) {
-                self.$searchview_buttons.find('[data-filter="'+k.filter+'"]').addClass('selected');
-            }
-        });
-        _.each(this.$searchview_buttons.find('.js_account_report_bool_filter'), function(k) {
-            $(k).toggleClass('selected', self.report_options[$(k).data('filter')]);
-        });
-        _.each(this.$searchview_buttons.find('.js_account_report_choice_filter'), function(k) {
-            $(k).toggleClass('selected', (_.filter(self.report_options[$(k).data('filter')], function(el){return ''+el.id == ''+$(k).data('id') && el.selected === true;})).length > 0);
-        });
-        $('.js_account_report_group_choice_filter', this.$searchview_buttons).each(function (i, el) {
-            var $el = $(el);
-            var ids = $el.data('member-ids');
-            $el.toggleClass('selected', _.every(self.report_options[$el.data('filter')], function (member) {
-                // only look for actual ids, discard separators and section titles
-                if(typeof member.id == 'number'){
-                  // true if selected and member or non member and non selected
-                  return member.selected === (ids.indexOf(member.id) > -1);
-                } else {
-                  return true;
-                }
-            }));
-        });
-        _.each(this.$searchview_buttons.find('.js_account_reports_one_choice_filter'), function(k) {
-            $(k).toggleClass('selected', ''+self.report_options[$(k).data('filter')] === ''+$(k).data('id'));
-        });
-        // click events
-        this.$searchview_buttons.find('.js_account_report_date_filter').click(function (event) {
-            self.report_options.date.filter = $(this).data('filter');
-            var error = false;
-            if ($(this).data('filter') === 'custom') {
-                var date_from = self.$searchview_buttons.find('.o_datepicker_input[name="date_from"]');
-                var date_to = self.$searchview_buttons.find('.o_datepicker_input[name="date_to"]');
-                if (date_from.length > 0){
-                    error = date_from.val() === "" || date_to.val() === "";
-                    self.report_options.date.date_from = field_utils.parse.date(date_from.val());
-                    self.report_options.date.date_to = field_utils.parse.date(date_to.val());
-                }
-                else {
-                    error = date_to.val() === "";
-                    self.report_options.date.date_to = field_utils.parse.date(date_to.val());
-                }
-            }
-            if (error) {
-                new WarningDialog(self, {
-                    title: _t("Odoo Warning"),
-                }, {
-                    message: _t("Date cannot be empty")
-                }).open();
-            } else {
-                self.reload();
-            }
-        });
-        this.$searchview_buttons.find('.js_account_report_bool_filter').click(function (event) {
-            var option_value = $(this).data('filter');
-            self.report_options[option_value] = !self.report_options[option_value];
-            if (option_value === 'unfold_all') {
-                self.unfold_all(self.report_options[option_value]);
-            }
-            self.reload();
-        });
-        $('.js_account_report_group_choice_filter', this.$searchview_buttons).click(function () {
-            var option_value = $(this).data('filter');
-            var option_member_ids = $(this).data('member-ids') || [];
-            var is_selected = $(this).hasClass('selected');
-            _.each(self.report_options[option_value], function (el) {
-                // if group was selected, we want to uncheck all
-                el.selected = !is_selected && (option_member_ids.indexOf(Number(el.id)) > -1);
-            });
-            self.reload();
-        });
-        this.$searchview_buttons.find('.js_account_report_choice_filter').click(function (event) {
-            var option_value = $(this).data('filter');
-            var option_id = $(this).data('id');
-            _.filter(self.report_options[option_value], function(el) {
-                if (''+el.id == ''+option_id){
-                    if (el.selected === undefined || el.selected === null){el.selected = false;}
-                    el.selected = !el.selected;
-                } else if (option_value === 'ir_filters') {
-                    el.selected = false;
-                }
-                return el;
-            });
-            self.reload();
-        });
-        var rate_handler = function (event) {
-            var option_value = $(this).data('filter');
-            if (option_value == 'current_currency') {
-                delete self.report_options.currency_rates;
-            } else if (option_value == 'custom_currency') {
-                _.each($('input.js_account_report_custom_currency_input'), function(input) {
-                    self.report_options.currency_rates[input.name].rate = input.value;
-                });
-            }
-            self.reload();
-        }
-        $(document).on('click', '.js_account_report_custom_currency', rate_handler);
-        this.$searchview_buttons.find('.js_account_report_custom_currency').click(rate_handler);
-        this.$searchview_buttons.find('.js_account_reports_one_choice_filter').click(function (event) {
-            self.report_options[$(this).data('filter')] = $(this).data('id');
-            self.reload();
-        });
-        this.$searchview_buttons.find('.js_account_report_date_cmp_filter').click(function (event) {
-            self.report_options.comparison.filter = $(this).data('filter');
-            var error = false;
-            var number_period = $(this).parent().find('input[name="periods_number"]');
-            self.report_options.comparison.number_period = (number_period.length > 0) ? parseInt(number_period.val()) : 1;
-            if ($(this).data('filter') === 'custom') {
-                var date_from = self.$searchview_buttons.find('.o_datepicker_input[name="date_from_cmp"]');
-                var date_to = self.$searchview_buttons.find('.o_datepicker_input[name="date_to_cmp"]');
-                if (date_from.length > 0) {
-                    error = date_from.val() === "" || date_to.val() === "";
-                    self.report_options.comparison.date_from = field_utils.parse.date(date_from.val());
-                    self.report_options.comparison.date_to = field_utils.parse.date(date_to.val());
-                }
-                else {
-                    error = date_to.val() === "";
-                    self.report_options.comparison.date_to = field_utils.parse.date(date_to.val());
-                }
-            }
-            if (error) {
-                new WarningDialog(self, {
-                    title: _t("Odoo Warning"),
-                }, {
-                    message: _t("Date cannot be empty")
-                }).open();
-            } else {
-                self.reload();
-            }
-        });
-
         // partner filter
         if (this.report_options.partner) {
             if (!this.M2MFilters) {
@@ -632,14 +465,14 @@ var accountReportsWidget = AbstractAction.extend({
                 var fields = {};
                 if (this.report_options.analytic_accounts) {
                     fields['analytic_accounts'] = {
-                        label: _t('Accounts'),
+                        label: _t('Branchs'),
                         modelName: 'res.branch',
                         value: this.report_options.analytic_accounts.map(Number),
                     };
                 }
                 if (this.report_options.analytic_tags) {
                     fields['analytic_tags'] = {
-                        label: _t('Tags'),
+                        label: _t('Branch Tags'),
                         modelName: 'branch.analytic.tag',
                         value: this.report_options.analytic_tags.map(Number),
                     };
@@ -698,7 +531,7 @@ var accountReportsWidget = AbstractAction.extend({
         var self = this;
         var text = $(e.target).siblings().val().replace(/[ \t]+/g, ' ');
         return this._rpc({
-                model: 'branch.report.manager',
+                model: 'account.report.manager',
                 method: 'write',
                 args: [this.report_manager_id, {summary: text}],
                 context: this.odoo_context,
@@ -770,7 +603,7 @@ var accountReportsWidget = AbstractAction.extend({
                 }
                 // replace text of existing footnote
                 return this._rpc({
-                        model: 'branch.report.footnote',
+                        model: 'account.report.footnote',
                         method: 'write',
                         args: [existing_footnote[0].id, {text: footnote_text}],
                         context: this.odoo_context,
@@ -787,7 +620,7 @@ var accountReportsWidget = AbstractAction.extend({
             else {
                 // new footnote
                 return this._rpc({
-                        model: 'branch.report.footnote',
+                        model: 'account.report.footnote',
                         method: 'create',
                         args: [{line: line_id, text: footnote_text, manager_id: self.report_manager_id}],
                         context: this.odoo_context,
@@ -804,7 +637,7 @@ var accountReportsWidget = AbstractAction.extend({
         var self = this;
         var footnote_id = $(e.target).parents('.footnote').data('id');
         return this._rpc({
-                model: 'branch.report.footnote',
+                model: 'account.report.footnote',
                 method: 'unlink',
                 args: [footnote_id],
                 context: this.odoo_context,
@@ -939,24 +772,11 @@ var accountReportsWidget = AbstractAction.extend({
         }
     },
 
-    //-------------------------------------------------------------------------
-    // Handlers
-    //-------------------------------------------------------------------------
-
-    /**
-     * When clicking inside a dropdown to modify search options
-     * prevents the bootstrap dropdown to close on itself
-     *
-     * @private
-     * @param {$.Event} ev
-     */
-    _onClickDropDownMenu: function (ev) {
+        _onClickDropDownMenu: function (ev) {
         ev.stopPropagation();
     },
 });
 
-core.action_registry.add('account_report', accountReportsWidget);
-
+core.action_registry.add('branch_report', accountReportsWidget);
 return accountReportsWidget;
-
 });
