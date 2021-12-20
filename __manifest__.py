3eae6752c801abe9b6e2e659d3bc4616920ed1f1
dev1-pcp <mahmudamen@gmail.com>
2021-12-20 17:06:27 +0200

    no error

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['branch','branch_accounting_report'],
+    'depends': ['account','branch','branch_accounting_report'],
 
     # always loaded
     'data': [
